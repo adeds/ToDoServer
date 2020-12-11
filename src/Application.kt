e7aabdba559a6a558e45f7864fc21b44ec4a2eb7
adeds <adeyds@gmail.com>
2020-12-11 21:14:34 +0700

    Authenticating and Config User in Application

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,10 @@
 package id.ade
 
+import id.ade.auth.JwtService
+import id.ade.auth.MySession
+import id.ade.auth.hash
+import id.ade.databse.DatabaseFactory
+import id.ade.repository.TodoRepository
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -8,6 +13,7 @@ import io.ktor.http.*
 import io.ktor.locations.*
 import io.ktor.sessions.*
 import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.gson.*
 import io.ktor.features.*
 
@@ -16,6 +22,14 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    // 1
+    DatabaseFactory.init()
+    val db = TodoRepository()
+// 2
+    val jwtService = JwtService()
+    val hashFunction = { s: String -> hash(s) }
+
+
     install(Locations) {
     }
 
@@ -26,6 +40,27 @@ fun Application.module(testing: Boolean = false) {
     }
 
     install(Authentication) {
+        jwt("jwt") {
+
+            //specifies the verifier you created in the JwtService class.
+            verifier(jwtService.verifier)
+            realm = "Todo Server"
+
+            //creates a method that runs each time the app needs to authenticate a call.
+            validate {
+                val payload = it.payload
+                val claim = payload.getClaim("id")
+                val claimString = claim.asInt()
+                val user = db.findUser(claimString)
+                /*
+                * tries to find the user in the database with the userId from claimString.
+                * If the userID exists, it verifies the user.
+                * Otherwise, it returns a null user and rejects the route.
+                * */
+                user
+            }
+        }
+
     }
 
     install(ContentNegotiation) {
@@ -37,6 +72,3 @@ fun Application.module(testing: Boolean = false) {
 
     }
 }
-
-data class MySession(val count: Int = 0)
-
--- src/util/Constant.kt
+++ src/util/Constant.kt
@@ -2,15 +2,19 @@ package id.ade.util
 
 class Constant {
 
+    object General{
+        const val PROJECT_NAME = "todoServer"
+    }
+
     object DatabaseKey {
-        object User{
+        object User {
             const val KEY_ID = "id"
             const val KEY_EMAIL = "email"
             const val KEY_DISPLAY_NAME = "display_name"
             const val KEY_PASSWORD_HASH = "password_hash"
         }
 
-        object Todo{
+        object Todo {
             const val KEY_ID = "id"
             const val KEY_USER_ID = "userId"
             const val KEY_TODO = "todo"
@@ -18,8 +22,8 @@ class Constant {
         }
     }
 
-    object Environment{
-        object DataBase{
+    object Environment {
+        object DataBase {
             const val JDBC_DRIVER = "JDBC_DRIVER"
             const val JDBC_DATABASE_URL = "JDBC_DATABASE_URL"
             const val TRANSACTION_REPEATABLE_READ = "TRANSACTION_REPEATABLE_READ"
@@ -27,6 +31,13 @@ class Constant {
             // doesn't use in local, only when deployed
             const val DB_NAME = "DB_USER"
             const val DB_PASSWORD = "DB_PASSWORD"
+        }
+
+        object Auth {
+            const val SECRET_KEY = "SECRET_KEY"
+            const val HASH_ALGORITH = "HmacSHA1"
+            const val JWT_SECRET = "JWT_SECRET"
+            const val AUTHENTICATION = "Authentication"
         }
     }
 }
