a30f47b924d7c40c425f98c70f33aabdfd6e826c
adeds <adeyds@gmail.com>
2020-12-11 19:54:27 +0700

    Getting started

---
+++ src/Application.kt
@@ -0,0 +1,42 @@
+package id.ade
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.sessions.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations) {
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+
+    }
+}
+
+data class MySession(val count: Int = 0)
+
